d8f0695ae6351bad0735979b61c56b81b00884ce
Marco Franco <piratafranco20@gmail.com>
2023-05-18 02:53:14 -0700

    Added pages folder and admin page

---
+++ pages/admin/adminPage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require("../../Fragments/adminHead.php"); ?>
+
+<body>
+  <?php require("../../Fragments/adminNavbar.php"); ?>
+  <h2 class="content_title text-center mt-3">Gestion de productos</h2>
+  <div class="text-center d-flex justify-content-center">
+    <div class="border align-items-center d-flex" style="
+      width: 201px;
+      height: 30px;
+      justify-content: space-between;
+      border-radius: 27px;
+      border-color: #404146;
+    ">
+      <a href="./addCategory.php" class="nav-link" style="margin-left: 14px;">Categoria</a>
+      <a href="./addProduct.php" class="nav-link" style="margin-right: 14px;">Productos</a>
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/4b777ceb0e.js" crossorigin="anonymous"></script>
+</body>
+
+</html>

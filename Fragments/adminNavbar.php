b819577279d4c29742b1e5056f0b7b8a0b4468ab
Marco Franco <piratafranco20@gmail.com>
2023-05-18 02:52:44 -0700

    Added Fragments folder and some fragments in it

---
+++ Fragments/adminNavbar.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg border-bottom">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a href="#" class="navbar-brand">
+      <img src="../../images/logo.png" alt="" id="logo" height="33px" width="47px">
+    </a>
+    <a class="navbar-brand" href="#">MascoTienda - Administración</a>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item mx-4">
+          <a class="nav-link" aria-current="page" href="#">Gestion de productos</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-5 ms-auto">
+        <li class="nav-item mx">
+          <a href="#" class="nav-link"><i class="fas fa-user" style="color:#000000"></i></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ Fragments/products.php
@@ -0,0 +1,44 @@
+<div class="container mt-5">
+  <div class="row d-flex" id="products_row">
+    <div class="col-3">
+      <div class="card" style="width: 18rem;">
+        <img src="./images/shutterstock_482660392.jpg" class="card-img-top" alt="...">
+        <div class="card-body text-center">
+          <h5 class="card-title">Peluche elefante</h5>
+          <p class="card-text">$269</p>
+          <a href="#" class="btn btn-primary">Agregar al carrito</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-3">
+      <div class="card" style="width: 18rem;">
+        <img src="./images/dog_category.jpg" class="card-img-top" alt="...">
+        <div class="card-body text-center">
+          <h5 class="card-title">Peluche elefante</h5>
+          <p class="card-text">$269</p>
+          <a href="#" class="btn btn-primary">Agregar al carrito</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-3">
+      <div class="card" style="width: 18rem;">
+        <img src="./images/cat_category.jpg" class="card-img-top" alt="...">
+        <div class="card-body text-center">
+          <h5 class="card-title">Peluche elefante</h5>
+          <p class="card-text">$269</p>
+          <a href="#" class="btn btn-primary">Agregar al carrito</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-3">
+      <div class="card" style="width: 18rem;">
+        <img src="./images/fish_category.jpg" class="card-img-top" alt="...">
+        <div class="card-body text-center">
+          <h5 class="card-title">Peluche elefante</h5>
+          <p class="card-text">$269</p>
+          <a href="#" class="btn btn-primary">Agregar al carrito</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
